7e75a83424f6fc5ddd78a09e4b3b63b465c595f7
rizqirahardian <rizqirahardianjos@gmail.com>
2019-12-01 14:09:54 +0700

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,38 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'angular-app';
+  accounts;
+  username;
+  password;
+
+  constructor(private httpClient: HttpClient) { }
+
+  ngOnInit() {
+    this.apiGet().subscribe((res)=>{
+      this.accounts = res
+      console.log(res)
+    })
+  }
+
+  public apiGet() {
+    return this.httpClient.get(`http://127.0.0.1:3000/getAccount`);
+  }
+
+  public apiPost(username, password) {
+    let data = {
+      username: username,
+      password: password
+    }
+    return this.httpClient.post(`http://127.0.0.1:3000/addAccount`, data)
+    .toPromise()
+    .then((res) => this.accounts.push(data))
+    .catch((err)=> console.error(err))
+  }
 }
